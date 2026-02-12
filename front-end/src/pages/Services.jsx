53506f7d44535e414006d9bddbcc5e8cddb4409e
Tsedenia <tsedenianega@gmail.com>
2026-02-12 22:15:37 +0300

    service fronted

--- front-end/src/pages/Services.jsx
+++ front-end/src/pages/Services.jsx
@@ -5,20 +5,10 @@ import "tailwindcss";
 function Services() {
   return (
     <div className=" flex flex-col items-center justify-center py-12">
-      {/* Cards and Dots Container */}
-      {/* <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-50"> */}
-
-        {/* Cards */}
+     
+       
         <Card />
-        {/* <Card /> */}
-        {/* <Card /> */}
-        {/* </div> */}
-
-        {/* Pagination Dots */}
-        {/* <div className="col-span-2 md:col-start-2 flex justify-center">
         
-          <Dots />
-        </div> */}
       
     </div>
   );
